89927706c64b88298a32ef404164569609c7b0ad
Himanshi-200 <himanshiaggarwal20060096018@gmail.com>
2024-12-01 10:44:02 +0530

    Add Project

---
+++ src/main/java/com/clicknshop/ClickNShopApplication.java
@@ -0,0 +1,14 @@
+package com.clicknshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickNShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickNShopApplication.class, args);
+	}
+}
+
+//Now convert into Microservices
